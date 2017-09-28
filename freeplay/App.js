a36fed3b5007a5e28e630fca0914fbfa0ddbb3ad
João Anes <joaoanes@lifeonmars.pt>
2017-09-28 16:23:14 +0100

    Checkmax

--- freeplay/App.js
+++ freeplay/App.js
@@ -7,11 +7,35 @@ const gyroObservable = new Gyroscope()
 
 export default class App extends React.Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      maxY : 0,
+      y: 0
+    }
+
+    gyroObservable
+    .map(({x, y, z}) => ({x: x*1000, y: y*1000, z: z*1000}))
+    .subscribe(({x, y, z}) =>
+      this.refs.ball.setVelocity({y: -1 * Math.abs(x+y+z)})
+    )
+  }
+
+  checkMax = ({nativeEvent: {x, y}}) => {
+    this.setState({y: -y+200})
+    if (-y+200 > this.state.maxY) {
+      this.setState({maxY: -y+200}) //y starts at 400
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={{position: 'absolute', top: 0, width: 25, height: 580, backgroundColor: 'grey'}} />
         <Interactable.View
+          ref='ball'
+          onDrag={this.checkMax}
           gravityPoints={[{x: 0, y: 200, strength: 150, falloff: 800, damping: 0.5}]}
           initialPosition={{x: 0, y: 400}}
           verticalOnly={true}
